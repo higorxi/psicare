ba4d8cb50b10a025b72ae558ba79cb3c9c808d11
Aluno FTT 01 <email@example.com>
2023-10-02 17:22:28 -0300

    criação arquitetura do visualizar dados

---
+++ src/routes/Admin/components/Visualizar/Secretario.tsx
@@ -0,0 +1,7 @@
+export default function VisualizarDadosAluno(){
+    return(
+        <>
+        <p>Aqui será o componente de visualização de dados dos secretários, haja vista que cada usuário tem suas informações específicas, a tela de visualização de dados deve fluir entre as informaçções disponiveis</p>
+        </>
+    );
+}
---
+++ src/routes/Admin/components/Visualizar/index.tsx
@@ -0,0 +1,17 @@
+export default function Visualizar(){
+    return(
+        <>
+        <p>Aqui será o componente de visualização de dados, a qual irá reunir todos os campos de visualização. Se deve seguir a arquitetura semelhante ao do componente Editar</p>
+        
+        /*criar visualizar dados para cada fluxo dentro da pasta Visualizar
+        reunir todos os Visualizar no index da pasta, para que consiga chamar esse componente lá no index page,
+
+
+
+        dúvidas:
+        necessito consumir os dados e os organizar no visualizar 
+        descobrir como colocar ícone de visualização no index page, e que ele ligue com o componente Visualizar*/
+        
+        </>
+    );
+}
